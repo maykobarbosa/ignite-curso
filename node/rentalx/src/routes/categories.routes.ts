aa3398e97b03992bbbe68a9f64b04c1e990c2637
maykobarbosa <67514858+maykobarbosa@users.noreply.github.com>
2021-10-05 18:49:51 -0300

    controller

--- node/rentalx/src/routes/categories.routes.ts
+++ node/rentalx/src/routes/categories.routes.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
 
-import { PostgresCategoriesRepository } from '../modules/cars/repositories/PostgresCategoriesRepository';
-import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { createCategoryController } from '../modules/cars/useCases/createCategory';
 
 const categoriesRoutes = Router();
-const categoriesRepository = new PostgresCategoriesRepository();
+const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-  return response.status(201).send();
-});
+categoriesRoutes.post('/', (request, response) => createCategoryController.handle(request.response));
 
 categoriesRoutes.get('/', (request, response) => {
   const all = categoriesRepository.list();
--- node/rentalx/src/routes/specifications.routes.ts
+++ node/rentalx/src/routes/specifications.routes.ts
@@ -8,6 +8,7 @@ const specificationsRoutes = Router();
 const specificationsRepository = new SpecificationRepository();
 
 specificationsRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
   const createSpecificationService = new CreateSpecificationService(specificationsRepository);
   createSpecificationService.execute({ name, description });
 
